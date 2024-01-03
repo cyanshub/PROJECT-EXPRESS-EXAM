68de5d4e126b919f7c3808f2d50cfc4c5fdcf857
travel15740 <travel15740@gmail.com>
2024-01-03 13:50:55 +0800

    撰寫導向各個頁面的路由

--- routes/modules/routes.js
+++ routes/modules/routes.js
@@ -9,6 +9,21 @@ router.get("/", (req,res)=>{
   res.render("index") // 渲染首頁
 })
 
+// 關於
+router.get("/about", (req,res)=>{
+  res.render("about") // 渲染其他樣板
+})
+
+// 作品集
+router.get("/portfolio", (req,res)=>{
+  res.render("portfolio") // 渲染其他樣板
+})
+
+// 聯絡
+router.get("/contact", (req,res)=>{
+  res.render("contact") // 渲染其他樣板
+})
+
 
 // 封包路由模組
 module.exports = router
